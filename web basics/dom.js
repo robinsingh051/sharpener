c2adfff1f971a6a4d19d75bed05e13e2933e0b20
robinsingh051 <robinsingh051s@gmail.com>
2023-08-14 21:08:35 +0530

    added dom task

---
+++ web basics/dom.js
@@ -0,0 +1,22 @@
+//console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title='New title';
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+let headerTitle=document.getElementById('header-title');
+// headerTitle.textContent='Hello';
+// headerTitle.innerHTML='Bye';
+
+headerTitle.style='border:1px solid black';
+
+let addItem=document.getElementById('add');
+console.log(addItem);
+addItem.style='font-weight:bold; color:green;';
