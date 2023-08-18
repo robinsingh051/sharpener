fe5b9558f407fd1e24b213954b92330153128fab
robinsingh051 <robinsingh051s@gmail.com>
2023-08-18 18:07:37 +0530

    added delete from crudcrud in booking app

--- web basics/booking app/main.js
+++ web basics/booking app/main.js
@@ -20,22 +20,6 @@ function onSubmit(e) {
     // Remove error after 3 seconds
     setTimeout(() => msg.remove(), 3000);
   } else {
-    // Add HTML
-    // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
-
-
-    // let details={
-    //     name:nameInput.value,
-    //     email:emailInput.value
-    // }
-    // let detailsSerialized=JSON.stringify(details);
-    // localStorage.setItem('credentials',detailsSerialized);
-
-    // // Retrieve existing credentials from local storage
-    // const existingCredentials = localStorage.getItem('credentials');
-
-    // // Parse the existing credentials if they exist
-    // const existingCredentialsParsed = existingCredentials ? JSON.parse(existingCredentials) : [];
 
     // Create new details object
     const newDetails = {
@@ -43,15 +27,6 @@ function onSubmit(e) {
         email: emailInput.value
     };
     showData(newDetails);
-
-    // // Add the new details to the existing credentials
-    // existingCredentialsParsed.push(newDetails);
-
-    // // Convert the updated credentials to a string
-    // const updatedCredentialsSerialized = JSON.stringify(existingCredentialsParsed);
-
-    // // Store the updated credentials in local storage
-    // localStorage.setItem('credentials', updatedCredentialsSerialized);
 
     // post to crud crud using axios
     axios.post('https://crudcrud.com/api/8806390386144251b3092ca3aad91050/user',newDetails)
@@ -62,16 +37,18 @@ function onSubmit(e) {
       console.error(err); 
     })
 
-
     // Clear fields
     nameInput.value = '';
     emailInput.value = '';
   }
 }
 
+//creating li with buttons
 function showData(obj){
   //create a li
   const li = document.createElement('li');
+  //give the id of obj to li which will help us while removing the data from crudcrud
+  li.id=obj._id;
   //create a delete button
   const delBtn=document.createElement('button');
   delBtn.appendChild(document.createTextNode('Delete'));
@@ -93,10 +70,7 @@ function showData(obj){
 
 const ul=document.getElementById('users');
 const delBtn=document.getElementsByClassName('del');
-// //add event listener to all delete buttons
-// for (let i = 0; i < delBtn.length; i++) {
-//     delBtn[i].addEventListener('click', removeItem);
-// }
+
 ul.addEventListener('click', removeItem);
 ul.addEventListener('click',editItem);
 
@@ -105,23 +79,32 @@ function removeItem(e){
         if(confirm('Are You Sure?')){
           var li = e.target.parentElement;
 
-          // Get the index of the item to delete
-          const index = Array.from(ul.children).indexOf(li);
+          // // Get the index of the item to delete
+          // const index = Array.from(ul.children).indexOf(li);
 
-          // Retrieve existing credentials from local storage
-          const existingCredentials = localStorage.getItem('credentials');
+          // // Retrieve existing credentials from local storage
+          // const existingCredentials = localStorage.getItem('credentials');
 
-          // Parse the existing credentials if they exist
-          const existingCredentialsParsed = existingCredentials ? JSON.parse(existingCredentials) : [];
+          // // Parse the existing credentials if they exist
+          // const existingCredentialsParsed = existingCredentials ? JSON.parse(existingCredentials) : [];
 
-          // Remove the item from the existing credentials array
-          existingCredentialsParsed.splice(index, 1);
+          // // Remove the item from the existing credentials array
+          // existingCredentialsParsed.splice(index, 1);
 
-          // Convert the updated credentials to a string
-          const updatedCredentialsSerialized = JSON.stringify(existingCredentialsParsed);
+          // // Convert the updated credentials to a string
+          // const updatedCredentialsSerialized = JSON.stringify(existingCredentialsParsed);
 
-          // Store the updated credentials in local storage
-          localStorage.setItem('credentials', updatedCredentialsSerialized);
+          // // Store the updated credentials in local storage
+          // localStorage.setItem('credentials', updatedCredentialsSerialized);
+
+          axios
+          .delete(`https://crudcrud.com/api/8806390386144251b3092ca3aad91050/user/${li.id}`)
+          .then((response)=>{
+            console.log(response);
+          })
+          .catch((err)=>{
+            console.log(err);
+          });
 
           ul.removeChild(li);
 
