7a71d89b8b14f64b361a007d06c747b66b39748e
robinsingh051 <robinsingh051s@gmail.com>
2023-08-18 13:16:01 +0530

    added axios task 1

--- test.js
+++ test.js
@@ -1,13 +1,18 @@
-const obj= {
-
-  "key1": "value1",
-  
-  "key2" : "value2",
-  
-  "key3" : "value3"
-  
-  }
-  
-  const obj2 = { ...obj}
-  
-  console.log(obj2 === obj)
+async function todo(){
+  console.log('a');
+  console.log('b');
+  let msg1=await new Promise((res,rej)=>{
+    setTimeout(()=>{
+      res('c');
+    },1000);
+  });
+  console.log(msg1);
+  let msg2=await new Promise((res,rej)=>{
+    setTimeout(()=>{
+      res('d');
+    },0);
+  });
+  console.log(msg2);
+  console.log('e');
+}
+todo();
