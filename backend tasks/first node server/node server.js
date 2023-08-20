90496808357c6931b52af5ca249235fa28dd8702
robinsingh051 <robinsingh051s@gmail.com>
2023-08-20 17:37:25 +0530

    added routing in first node server using http

--- backend tasks/first node server/node server.js
+++ backend tasks/first node server/node server.js
@@ -1,7 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('My name is Robin Singh');
+  //res.end('<h1>Welcome to my Node Js project</h1>');
+  if (req.url === '/home') {
+    res.end('<h1>Welcome home</h1>');
+  } else if (req.url === '/about') {
+    res.end('<h1>Welcome to About Us page</h1>');
+  } else if (req.url === '/node') {
+    res.end('<h1>Welcome to my Node Js project</h1>');
+  } else {
+    res.end('<h1>Page not found</h1>');
+  }
 });
 
 const port = 4000;
