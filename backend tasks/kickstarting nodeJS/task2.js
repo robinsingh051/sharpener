3c90c026cd845f158825daa0b0d5cc6a998e6d0c
robinsingh051 <robinsingh051s@gmail.com>
2023-08-20 09:39:07 +0530

    added revise javascript concepts task

---
+++ backend tasks/kickstarting nodeJS/task2.js
@@ -0,0 +1,12 @@
+const mul=(a,b)=>{
+    return a*b;
+}
+console.log(mul(10,2));
+
+const student={
+    name: 'Robin Singh',
+    age: 22,
+    education:'BE',
+    location: 'Bangalore'
+}
+console.log(student);
