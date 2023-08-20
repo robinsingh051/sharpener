3f7e5dc33b35604c2d7140b9bfea84e9d945d5be
robinsingh051 <robinsingh051s@gmail.com>
2023-08-20 09:03:37 +0530

    added hello world code[C

--- rest api test/main.js
+++ rest api test/main.js
@@ -1,19 +1,19 @@
 // Put DOM elements into variables
 const myForm = document.querySelector('#my-form');
-const priceInput=document.querySelector('#price')
+const priceInput = document.querySelector('#price');
 const nameInput = document.querySelector('#name');
 const catInput = document.querySelector('#category');
 const msg = document.querySelector('.msg');
 const electronicsList = document.querySelector('#ele');
-const foodList=document.querySelector('#foo');
-const fashionList=document.querySelector('#fas');
+const foodList = document.querySelector('#foo');
+const fashionList = document.querySelector('#fas');
 
 // Listen for form submit
 myForm.addEventListener('submit', onSubmit);
 
-function onSubmit(e) {
+async function onSubmit(e) {
   e.preventDefault();
-  if(nameInput.value === '' || priceInput.value === '' || catInput.value==='') {
+  if (nameInput.value === '' || priceInput.value === '' || catInput.value === '') {
     msg.classList.add('error');
     msg.textContent = 'Please enter all fields';
 
@@ -23,36 +23,35 @@ function onSubmit(e) {
 
     // Create new details object
     const newItem = {
-        price:priceInput.value,
-        name: nameInput.value,
-        category: catInput.value
+      price: priceInput.value,
+      name: nameInput.value,
+      category: catInput.value
     };
-    showData(newItem);
+    await showData(newItem);
 
-    // post to crud crud using axios
-    axios.post('https://crudcrud.com/api/8806390386144251b3092ca3aad91050/item',newItem)
-    .then(res => {
-      console.log(res)
-    })
-    .catch(err => {
-      console.error(err); 
-    })
+    try {
+      // post to crudcrud using axios
+      const response = await axios.post('https://crudcrud.com/api/ad4b75532f2d469c95538db1e0d9561e/item', newItem);
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
 
     // Clear fields
-    priceInput.value='';
+    priceInput.value = '';
     nameInput.value = '';
     catInput.value = '';
   }
 }
 
 //creating li with buttons
-function showData(obj){
+async function showData(obj) {
   //create a li
   const li = document.createElement('li');
   //give the id of obj to li which will help us while removing the data from crudcrud
-  li.id=obj._id;
+  li.id = obj._id;
   //create a delete button
-  const delBtn=document.createElement('button');
+  const delBtn = document.createElement('button');
   delBtn.appendChild(document.createTextNode('Delete'));
   delBtn.classList.add('del');
 
@@ -61,50 +60,41 @@ function showData(obj){
   // add button to li
   li.appendChild(delBtn);
   // Append to ul
-  if(obj.category==='electronics')
+  if (obj.category === 'electronics')
     electronicsList.appendChild(li);
-  if(obj.category==='food')
-  foodList.appendChild(li);
-  if(obj.category==='fashion')
-  fashionList.appendChild(li);
-  
+  if (obj.category === 'food')
+    foodList.appendChild(li);
+  if (obj.category === 'fashion')
+    fashionList.appendChild(li);
 }
 
-
 electronicsList.addEventListener('click', removeItem);
-foodList.addEventListener('click',removeItem);
-fashionList.addEventListener('click',removeItem);
+foodList.addEventListener('click', removeItem);
+fashionList.addEventListener('click', removeItem);
 
-function removeItem(e){
-    if(e.target.classList.contains('del')){
-        if(confirm('Are You Sure?')){
-          var li = e.target.parentElement;
-          var ul=li.parentElement;
-          axios
-          .delete(`https://crudcrud.com/api/8806390386144251b3092ca3aad91050/item/${li.id}`)
-          .then((response)=>{
-            console.log(response);
-          })
-          .catch((err)=>{
-            console.log(err);
-          });
-
-          ul.removeChild(li);
-
-        }
+async function removeItem(e) {
+  if (e.target.classList.contains('del')) {
+    if (confirm('Are You Sure?')) {
+      const li = e.target.parentElement;
+      const ul = li.parentElement;
+      try {
+        await axios.delete(`https://crudcrud.com/api/ad4b75532f2d469c95538db1e0d9561e/item/${li.id}`);
+        ul.removeChild(li);
+      } catch (err) {
+        console.log(err);
       }
+    }
+  }
 }
 
 //Run the function after the script is loaded in the browser
-document.addEventListener('DOMContentLoaded',()=>{
-  axios
-  .get('https://crudcrud.com/api/8806390386144251b3092ca3aad91050/item')
-  .then((response)=>{
-    for(let i=0;i<response.data.length;i++){
-      showData(response.data[i]);
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    const response = await axios.get('https://crudcrud.com/api/ad4b75532f2d469c95538db1e0d9561e/item');
+    for (let i = 0; i < response.data.length; i++) {
+      await showData(response.data[i]);
     }
-  })
-  .catch((err)=>{
+  } catch (err) {
     console.log(err);
-  });
+  }
 });
