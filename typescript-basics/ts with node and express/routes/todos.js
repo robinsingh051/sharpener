f6b681283a85e0e2c24d36e16895bdab5fda6488
robinsingh051 <robinsingh051s@gmail.com>
2023-09-04 17:06:55 +0530

    typescript with node and express

---
+++ typescript-basics/ts with node and express/routes/todos.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+let todos = [];
+const router = (0, express_1.Router)();
+router.get("/", (req, res, next) => {
+    res.status(200).json({ todos: todos });
+});
+router.post("/todo", (req, res, next) => {
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: req.body.text,
+    };
+    res.status(201).json({ message: "Todo Added", todo: newTodo });
+});
+router.put("/todo/:todoId", (req, res, next) => {
+    const tid = req.params.todoId;
+    const todoIndex = todos.findIndex((todoItem) => todoItem.id === tid);
+    if (todoIndex >= 0) {
+        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+        return res.status(200).json({ message: "todo updated", todos: todos });
+    }
+    res.status(404).json({ message: "todo not fount" });
+});
+router.delete("/todo/:todoId", (req, res, next) => {
+    todos = todos.filter((todoItem) => todoItem.id !== req.params.todoId);
+    res.status(200).json({ message: "todo deleted", todos: todos });
+});
+exports.default = router;
