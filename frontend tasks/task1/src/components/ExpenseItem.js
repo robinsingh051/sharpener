86df2d5706a8f3307f63e95d39493f126ca3ee59
robinsingh051 <robinsingh051s@gmail.com>
2023-09-21 11:26:24 +0530

    Styling and using JS in JSX in frontend task1

--- frontend tasks/task1/src/components/ExpenseItem.js
+++ frontend tasks/task1/src/components/ExpenseItem.js
@@ -1,10 +1,17 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 9, 21);
+  const expenseTitle = "Food";
+  const expenseAmount = 10;
+  const locationOfExpense = "Kanpur";
   return (
-    <div>
-      <div>September 21st 2023</div>
-      <div>
-        <h2>Food</h2>
-        <div>Rs 10</div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h2>{locationOfExpense}</h2>
+        <div className="expense-item__price">Rs {expenseAmount}</div>
       </div>
     </div>
   );
