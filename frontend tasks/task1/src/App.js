1c9ba2bcd3f5f6bf2705fd0fa3bde80024e17ce8
robinsingh051 <robinsingh051s@gmail.com>
2023-09-21 11:51:29 +0530

    Passing Data concept in frontend task1

--- frontend tasks/task1/src/App.js
+++ frontend tasks/task1/src/App.js
@@ -1,9 +1,40 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    {
+      title: "Car Insurance",
+      amount: 15000,
+      date: new Date(2023, 5, 25),
+      location: "Kanpur",
+    },
+    {
+      title: "Blood Test",
+      amount: 260,
+      date: new Date(2023, 6, 26),
+      location: "Bangalore",
+    },
+    {
+      title: "Lunch",
+      amount: 1400,
+      date: new Date(2023, 7, 27),
+      location: "Lucknow",
+    },
+    {
+      title: "Mobile recharge",
+      amount: 749,
+      date: new Date(2023, 8, 28),
+      location: "kanpur",
+    },
+  ];
+
+  const ExpenseItems = [];
+  for (let expense of expenses) {
+    ExpenseItems.push(<ExpenseItem expense={expense}></ExpenseItem>);
+  }
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      {ExpenseItems};
     </div>
   );
 }
--- frontend tasks/task1/src/components/ExpenseItem.js
+++ frontend tasks/task1/src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 9, 21);
-  const expenseTitle = "Food";
-  const expenseAmount = 10;
-  const locationOfExpense = "Kanpur";
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.expense.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <h2>{locationOfExpense}</h2>
-        <div className="expense-item__price">Rs {expenseAmount}</div>
+        <h2>{props.expense.title}</h2>
+        <h2>{props.expense.location}</h2>
+        <div className="expense-item__price">Rs {props.expense.amount}</div>
       </div>
     </div>
   );
