278d1c4f2e66d879590a9f7174f43f8c9b6f2194
robinsingh051 <robinsingh051s@gmail.com>
2023-09-21 10:13:14 +0530

    created ExpenseItem component in frontend task1

---
+++ frontend tasks/task1/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>September 21st 2023</div>
+      <div>
+        <h2>Food</h2>
+        <div>Rs 10</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
