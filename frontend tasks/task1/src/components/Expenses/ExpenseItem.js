8091d83e602b9de7a3482730c3a07e05761bb1e3
robinsingh051 <robinsingh051s@gmail.com>
2023-09-21 21:17:13 +0530

    added update title button for each expenseItem in frontend task1

--- frontend tasks/task1/src/components/Expenses/ExpenseItem.js
+++ frontend tasks/task1/src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,25 @@
+import React, { useState } from "react";
+
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.expense.title);
   const clickHandler = () => {
-    console.log(props.expense.id);
-    alert("clicked!!!");
-    const div = document.getElementById(props.expense.id);
-    div.remove();
+    setTitle("Updated!!!");
+    // alert("clicked!!!");
+    // const div = document.getElementById(props.expense.id);
+    // div.remove();
   };
   return (
     <Card className="expense-item" id={props.expense.id}>
       <ExpenseDate date={props.expense.date} />
       <div className="expense-item__description">
-        <h2>{props.expense.title}</h2>
+        <h2>{title}</h2>
         <h2>{props.expense.location}</h2>
         <div className="expense-item__price">Rs {props.expense.amount}</div>
       </div>
-      <button onClick={clickHandler}>Delete</button>
+      <button onClick={clickHandler}>Update Title</button>
     </Card>
   );
 };
