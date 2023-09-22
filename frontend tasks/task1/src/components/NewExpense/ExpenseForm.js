d7c94f8fe2e7bcc62a3550457386d6ec157e1a0c
robinsingh051 <robinsingh051s@gmail.com>
2023-09-22 06:07:51 +0530

    introduced state inside new Expense component in frontend task1

--- frontend tasks/task1/src/components/NewExpense/ExpenseForm.js
+++ frontend tasks/task1/src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,23 @@
+import React, { useState } from "react";
+
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
   const titleChangeHandler = (event) => {
+    setEnteredTitle(event.input.value);
+    console.log(event.target.value);
+  };
+
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.input.value);
+    console.log(event.target.value);
+  };
+
+  const [enteredDate, setEnteredDate] = useState("");
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.input.value);
     console.log(event.target.value);
   };
   return (
@@ -13,11 +29,16 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number"></input>
+          <input type="number" onChange={amountChangeHandler}></input>
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023-12-31"></input>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2023-12-31"
+            onChange={dateChangeHandler}
+          ></input>
         </div>
       </div>
       <div className="new-expense__actions">
