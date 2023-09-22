e3d256f0bda24a218503464f285c108a49325bd9
robinsingh051 <robinsingh051s@gmail.com>
2023-09-22 11:10:19 +0530

    completed Challenges on Conditional Rendering in frontend task1

--- frontend tasks/task1/src/components/NewExpense/ExpenseForm.js
+++ frontend tasks/task1/src/components/NewExpense/ExpenseForm.js
@@ -113,6 +113,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button type="button" onClick={props.onStopEditing}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
