bc74207ca121136bc784ee9b92feb8e6767e84f6
robinsingh051 <robinsingh051s@gmail.com>
2023-09-22 08:13:00 +0530

    key for each expense in frontend task1

--- frontend tasks/task1/src/components/Expenses/Expenses.js
+++ frontend tasks/task1/src/components/Expenses/Expenses.js
@@ -1,14 +1,31 @@
+import React, { useState } from "react";
+
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "../ExpenseFilter/ExpenseFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   const ExpenseItems = [];
   const expenses = props.expenses;
   for (let expense of expenses) {
-    ExpenseItems.push(<ExpenseItem expense={expense}></ExpenseItem>);
+    ExpenseItems.push(<ExpenseItem key={expense.id} expense={expense} />);
   }
-  return <Card className="expenses">{ExpenseItems}</Card>;
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {ExpenseItems}
+    </Card>
+  );
 };
 
 export default Expenses;
