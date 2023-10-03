e3f55b61df3f857a89c35e596da0e89081c302ad
robinsingh051 <robinsingh051s@gmail.com>
2023-10-03 17:46:55 +0530

    added password Reset functionality

--- frontend tasks/user-authentication/src/components/Profile/ProfileForm.js
+++ frontend tasks/user-authentication/src/components/Profile/ProfileForm.js
@@ -1,17 +1,57 @@
-import classes from './ProfileForm.module.css';
+import classes from "./ProfileForm.module.css";
+import React, { useRef, useState, useContext } from "react";
+import axios from "axios";
+import AuthContext from "../../store/auth-context";
 
 const ProfileForm = () => {
+  const authCtx = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [passwordReset, setPasswordReset] = useState(false);
+  const passwordInputRef = useRef();
+
+  const submitHandler = async (event) => {
+    setIsLoading(true);
+    setPasswordReset(false);
+    event.preventDefault();
+    const enteredPassword = passwordInputRef.current.value;
+    try {
+      const response = await axios.post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${process.env.REACT_APP_API_KEY}`,
+        {
+          idToken: authCtx.token,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }
+      );
+      console.log(response.data.idToken);
+      setPasswordReset(true);
+    } catch (err) {
+      let errorMessage = "Password Reset failed";
+      if (err.response.data.error && err.response.data.error.message)
+        errorMessage = err.response.data.error.message;
+      alert(errorMessage);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
+    <form className={classes.form} onSubmit={submitHandler}>
+      {passwordReset && <h1>Password Reset Successfull</h1>}
+      {!passwordReset && (
+        <div className={classes.control}>
+          <label htmlFor="new-password">New Password</label>
+          <input type="password" id="new-password" ref={passwordInputRef} />
+        </div>
+      )}
+      {!passwordReset && (
+        <div className={classes.action}>
+          {!isLoading && <button>Change Password</button>}
+          {isLoading && <p>Sending Request</p>}
+        </div>
+      )}
     </form>
   );
-}
+};
 
 export default ProfileForm;
